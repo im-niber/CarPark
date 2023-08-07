dcdb7d30f193e8920abc1b481ca900d544316d76
rbwo552 <rbwo552@naver.com>
2023-08-07 19:10:17 +0900

    [Refactor]: Publisher extension 추가, ViewModel 리팩토링

--- CarPark/ViwModels/JoinViewModel.swift
+++ CarPark/ViwModels/JoinViewModel.swift
@@ -1,34 +1,49 @@
 import Foundation
 import Combine
 
-final class JoinViewModel {
-    
-    typealias VerifyTuple = (id: Bool, nickName: Bool, email: Bool, password: Bool)
-    
-    @Published var verifys: VerifyTuple = (false, false, false, false)
-    
-    func checkid(_ bool: Bool) {
-        verifys.id = bool
+final class JoinViewModel: ViewModelType {
+    struct Input {
+        let id: AnyPublisher<String, Never>
+        let nickName: AnyPublisher<String, Never>
+        let email: AnyPublisher<String, Never>
+        let password: AnyPublisher<String, Never>
+        let password2: AnyPublisher<String, Never>
     }
     
-    func checkNickname(_ bool: Bool) {
-        verifys.nickName = bool
+    struct Output {
+        let emailIsValid: AnyPublisher<Bool, Never>
+        let passwrodIsValid: AnyPublisher<Bool, Never>
+        let buttonIsValid: AnyPublisher<Bool, Never>
     }
     
-    func checkPassword(_ bool: Bool) -> Bool {
-        verifys.password = bool
+    func transform(input: Input) -> Output {
+        let emailStatePublisher = input.email
+            .map { self.checkEmail($0) }
+            .eraseToAnyPublisher()
         
-        return bool
-    }
-    
-    func checkEmail(_ bool: Bool) -> Bool {
-        verifys.email = bool
+        let passwordStatePublisher = input.password.combineLatest(input.password2)
+            .map { self.checkPassword($0, $1) }
+            .eraseToAnyPublisher()
         
-        return bool
+        let textCount = input.id.combineLatest(input.nickName, input.email)
+        let passwordCheck = input.password.combineLatest(input.password2)
+        
+        let buttonStatePublisher = textCount.combineLatest(passwordCheck)
+            .map { idNicknameEmail, password in
+                idNicknameEmail.0.count > 0 && idNicknameEmail.1.count > 0 && self.checkEmail(idNicknameEmail.2) && self.checkPassword(password.0, password.1)
+            }
+            .eraseToAnyPublisher()
+    
+        return Output(emailIsValid: emailStatePublisher, passwrodIsValid: passwordStatePublisher, buttonIsValid: buttonStatePublisher)
     }
     
-    func checkJoinBtn(with verifys: VerifyTuple) -> Bool {
-        verifys.id && verifys.email && verifys.password && verifys.nickName
+    func checkEmail(_ str: String) -> Bool {
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        return str.count > 0 && NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: str)
+    }
+    
+    func checkPassword(_ str1: String, _ str2: String) -> Bool {
+        str1.count > 0 && str1 == str2
     }
     
     func joinAction(id: String?, pw: String?, email: String?, nickname: String?) {
